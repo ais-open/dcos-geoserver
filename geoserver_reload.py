13f6f16cd2b60e2d6fec6f4270fba37ed47b817f
Jonathan Meyer <jon@gisjedi.com>
2016-05-04 20:26:58 -0400

    Removed GeoServer authentication as it is no longer enforced

--- geoserver_reload.py
+++ geoserver_reload.py
@@ -5,8 +5,6 @@ from time import sleep
 from geoserver.catalog import Catalog
 
 RELOAD_INTERVAL = int(getenv('GS_RELOAD_INTERVAL', '5'))
-USERNAME = getenv('GS_USERNAME', 'admin')
-PASSWORD = getenv('GS_PASSWORD', 'geoserver')
 PROTOCOL = getenv('GS_PROTOCOL', 'http')
 RELATIVE_URL = getenv('GS_RELATIVE_URL', '/geoserver/rest')
 
@@ -16,8 +14,6 @@ def reload_config(instances):
 
     Optional environment variables:
     GS_RELOAD_INTERVAL: time in seconds to delay between slave instance reloads
-    GS_USERNAME: admin username
-    GS_PASSWORD: admin password
     GS_PROTOCOL: protocol prefix, should be set to 'HTTP' or 'HTTPS'
     GS_RELATIVE_URL: relative URL to GeoServer REST API
 
@@ -30,7 +26,7 @@ def reload_config(instances):
     for instance in instances:
         url = '%s://%s%s' % (PROTOCOL, instance, RELATIVE_URL)
         logging.info('Performing GeoServer configuration reload at: %s' % url)
-        catalog = Catalog(url, username=USERNAME, password=PASSWORD)
+        catalog = Catalog(url)
         result = catalog.reload()
         if result and result[0]:
             if 200 <= result[0].status < 300:
