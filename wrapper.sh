03325f68fcc990c35b09ad11bd420c1ea0692d1a
Jonathan Meyer <jon@gisjedi.com>
2016-05-05 14:28:49 -0400

    Extra commands to identify where container is dying

--- wrapper.sh
+++ wrapper.sh
@@ -3,6 +3,7 @@
 set -e
 
 python marathon_bootstrap.py
-
+echo Starting HTTP Server
 python -m SimpleHTTPServer 8000 &
-exec ./geoserver_watch.py
+echo Starting GeoServer data directory watch
+exec python geoserver_watch.py
